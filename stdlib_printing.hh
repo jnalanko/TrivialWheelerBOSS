1cb7e4b24170a6c4f792d993bee7718d6ccecfd4
CowKeyMan <dancauchi1@gmail.com>
2022-06-07 15:48:20 +0300

    Refactor stdlib_printing for extended scalability

--- stdlib_printing.hh
+++ stdlib_printing.hh
@@ -4,68 +4,66 @@
 #include <map>
 #include <vector>
 #include <set>
-#include <utility>
 #include <iostream>
 
 using namespace std;
 
+
+template <typename Structure, typename Function>
+ostream& print_all(
+  ostream& os,
+  const string prefix,
+  const Structure& v,
+  const Function print_item,
+  const string suffix
+) {
+  os << prefix;
+  for(auto it = v.begin(); it != v.end(); it++) {
+    if(it != v.begin()) os << ", ";
+    print_item(os, *it);
+  }
+  os << suffix;
+  return os;
+}
+
+template <typename T>
+void print_individual(ostream& os, const T& item) {
+  os << item;
+}
+
+template <typename T>
+void print_first_second(ostream& os, const T& item) {
+  os << item.first << ": " << item.second;
+}
+
+
 template <typename S, typename T>
 ostream& operator<<(ostream& os, const unordered_map<S,T>& v){
-    os << "[";
-    for(auto it = v.begin(); it != v.end(); it++) {
-        if(it != v.begin()) os << ", ";
-        os << it->first << ": " << it->second;
-    }
-    os << "]";
-    return os;
+  return print_all<typeof(v)>(os, "[", v, print_first_second<pair<S, T>>, "]");
 }
 
 template <typename S, typename T>
 ostream& operator<<(ostream& os, const map<S,T>& v){
-    os << "{";
-    for(auto it = v.begin(); it != v.end(); it++) {
-        if(it != v.begin()) os << ", ";
-        os << it->first << ": " << it->second;
-    }
-    os << "}";
-    return os;
+  return print_all<typeof(v)>(os, "{", v, print_first_second<pair<S, T>>, "}");
 }
 
 template <typename T>
 ostream& operator<<(ostream& os, const vector<T>& v){
-    os << "[";
-    for(auto it = v.begin(); it != v.end(); it++) {
-        if(it != v.begin()) os << ", ";
-        os << *it;
-    }
-    os << "]";
-    return os;
+  return print_all<typeof(v)>(os, "[", v, print_individual<T>, "]");
 }
 
 template <typename T>
 ostream& operator<<(ostream& os, const set<T>& v){
-    os << "[";
-    for(auto it = v.begin(); it != v.end(); it++) {
-        if(it != v.begin()) os << ", ";
-        os << *it;
-    }
-    os << "]";
-    return os;
+  return print_all<typeof(v)>(os, "[", v, print_individual<T>, "]");
 }
 
 template <typename T>
 ostream& operator<<(ostream& os, const multiset<T>& v){
-    os << "[";
-    for(auto it = v.begin(); it != v.end(); it++) {
-        if(it != v.begin()) os << ", ";
-        os << *it;
-    }
-    os << "]";
-    return os;
+  return print_all<typeof(v)>(os, "[", v, print_individual<T>, "]");
 }
 
 template <typename S, typename T>
 ostream& operator<<(ostream& os, const pair<S,T>& x){
-    os << "(" << x.first << ", " << x.second << ")";
-    return os;
+  os << "(" << x.first << ", " << x.second << ")";
+  return os;
 }
